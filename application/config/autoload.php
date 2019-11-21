82f88acf0e0bb38d72a8cc5ecb696e269230906b
muharriz <anand.sinaga@gmail.com>
2019-11-21 11:23:58 +0700

    Update daftar 1.0

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -3,94 +3,133 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 /*
 | -------------------------------------------------------------------
-| DATABASE CONNECTIVITY SETTINGS
+| AUTO-LOADER
 | -------------------------------------------------------------------
-| This file will contain the settings needed to access your database.
+| This file specifies which systems should be loaded by default.
 |
-| For complete instructions please consult the 'Database Connection'
-| page of the User Guide.
+| In order to keep the framework as light-weight as possible only the
+| absolute minimal resources are loaded by default. For example,
+| the database is not connected to automatically since no assumption
+| is made regarding whether you intend to use it.  This file lets
+| you globally define which systems you would like loaded with every
+| request.
 |
 | -------------------------------------------------------------------
-| EXPLANATION OF VARIABLES
+| Instructions
 | -------------------------------------------------------------------
 |
-|	['dsn']      The full DSN string describe a connection to the database.
-|	['hostname'] The hostname of your database server.
-|	['username'] The username used to connect to the database
-|	['password'] The password used to connect to the database
-|	['database'] The name of the database you want to connect to
-|	['dbdriver'] The database driver. e.g.: mysqli.
-|			Currently supported:
-|				 cubrid, ibase, mssql, mysql, mysqli, oci8,
-|				 odbc, pdo, postgre, sqlite, sqlite3, sqlsrv
-|	['dbprefix'] You can add an optional prefix, which will be added
-|				 to the table name when using the  Query Builder class
-|	['pconnect'] TRUE/FALSE - Whether to use a persistent connection
-|	['db_debug'] TRUE/FALSE - Whether database errors should be displayed.
-|	['cache_on'] TRUE/FALSE - Enables/disables query caching
-|	['cachedir'] The path to the folder where cache files should be stored
-|	['char_set'] The character set used in communicating with the database
-|	['dbcollat'] The character collation used in communicating with the database
-|				 NOTE: For MySQL and MySQLi databases, this setting is only used
-| 				 as a backup if your server is running PHP < 5.2.3 or MySQL < 5.0.7
-|				 (and in table creation queries made with DB Forge).
-| 				 There is an incompatibility in PHP with mysql_real_escape_string() which
-| 				 can make your site vulnerable to SQL injection if you are using a
-| 				 multi-byte character set and are running versions lower than these.
-| 				 Sites using Latin-1 or UTF-8 database character set and collation are unaffected.
-|	['swap_pre'] A default table prefix that should be swapped with the dbprefix
-|	['encrypt']  Whether or not to use an encrypted connection.
+| These are the things you can load automatically:
 |
-|			'mysql' (deprecated), 'sqlsrv' and 'pdo/sqlsrv' drivers accept TRUE/FALSE
-|			'mysqli' and 'pdo/mysql' drivers accept an array with the following options:
+| 1. Packages
+| 2. Libraries
+| 3. Drivers
+| 4. Helper files
+| 5. Custom config files
+| 6. Language files
+| 7. Models
 |
-|				'ssl_key'    - Path to the private key file
-|				'ssl_cert'   - Path to the public key certificate file
-|				'ssl_ca'     - Path to the certificate authority file
-|				'ssl_capath' - Path to a directory containing trusted CA certificates in PEM format
-|				'ssl_cipher' - List of *allowed* ciphers to be used for the encryption, separated by colons (':')
-|				'ssl_verify' - TRUE/FALSE; Whether verify the server certificate or not
-|
-|	['compress'] Whether or not to use client compression (MySQL only)
-|	['stricton'] TRUE/FALSE - forces 'Strict Mode' connections
-|							- good for ensuring strict SQL while developing
-|	['ssl_options']	Used to set various SSL options that can be used when making SSL connections.
-|	['failover'] array - A array with 0 or more data for connections if the main should fail.
-|	['save_queries'] TRUE/FALSE - Whether to "save" all executed queries.
-| 				NOTE: Disabling this will also effectively disable both
-| 				$this->db->last_query() and profiling of DB queries.
-| 				When you run a query, with this setting set to TRUE (default),
-| 				CodeIgniter will store the SQL statement for debugging purposes.
-| 				However, this may cause high memory usage, especially if you run
-| 				a lot of SQL queries ... disable this to avoid that problem.
-|
-| The $active_group variable lets you choose which connection group to
-| make active.  By default there is only one group (the 'default' group).
-|
-| The $query_builder variables lets you determine whether or not to load
-| the query builder class.
 */
-$active_group = 'default';
-$query_builder = TRUE;
 
-$db['default'] = array(
-	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'itfest3',
-	'dbdriver' => 'mysqli',
-	'dbprefix' => '',
-	'pconnect' => FALSE,
-	'db_debug' => (ENVIRONMENT !== 'production'),
-	'cache_on' => FALSE,
-	'cachedir' => '',
-	'char_set' => 'utf8',
-	'dbcollat' => 'utf8_general_ci',
-	'swap_pre' => '',
-	'encrypt' => FALSE,
-	'compress' => FALSE,
-	'stricton' => FALSE,
-	'failover' => array(),
-	'save_queries' => TRUE
-);
+/*
+| -------------------------------------------------------------------
+|  Auto-load Packages
+| -------------------------------------------------------------------
+| Prototype:
+|
+|  $autoload['packages'] = array(APPPATH.'third_party', '/usr/local/shared');
+|
+*/
+$autoload['packages'] = array();
+
+/*
+| -------------------------------------------------------------------
+|  Auto-load Libraries
+| -------------------------------------------------------------------
+| These are the classes located in system/libraries/ or your
+| application/libraries/ directory, with the addition of the
+| 'database' library, which is somewhat of a special case.
+|
+| Prototype:
+|
+|	$autoload['libraries'] = array('database', 'email', 'session');
+|
+| You can also supply an alternative library name to be assigned
+| in the controller:
+|
+|	$autoload['libraries'] = array('user_agent' => 'ua');
+*/
+$autoload['libraries'] = array('database', 'session', 'form_validation');
+
+/*
+| -------------------------------------------------------------------
+|  Auto-load Drivers
+| -------------------------------------------------------------------
+| These classes are located in system/libraries/ or in your
+| application/libraries/ directory, but are also placed inside their
+| own subdirectory and they extend the CI_Driver_Library class. They
+| offer multiple interchangeable driver options.
+|
+| Prototype:
+|
+|	$autoload['drivers'] = array('cache');
+|
+| You can also supply an alternative property name to be assigned in
+| the controller:
+|
+|	$autoload['drivers'] = array('cache' => 'cch');
+|
+*/
+$autoload['drivers'] = array();
+
+/*
+| -------------------------------------------------------------------
+|  Auto-load Helper Files
+| -------------------------------------------------------------------
+| Prototype:
+|
+|	$autoload['helper'] = array('url', 'file');
+*/
+$autoload['helper'] = array('url');
+
+/*
+| -------------------------------------------------------------------
+|  Auto-load Config files
+| -------------------------------------------------------------------
+| Prototype:
+|
+|	$autoload['config'] = array('config1', 'config2');
+|
+| NOTE: This item is intended for use ONLY if you have created custom
+| config files.  Otherwise, leave it blank.
+|
+*/
+$autoload['config'] = array();
+
+/*
+| -------------------------------------------------------------------
+|  Auto-load Language files
+| -------------------------------------------------------------------
+| Prototype:
+|
+|	$autoload['language'] = array('lang1', 'lang2');
+|
+| NOTE: Do not include the "_lang" part of your file.  For example
+| "codeigniter_lang.php" would be referenced as array('codeigniter');
+|
+*/
+$autoload['language'] = array();
+
+/*
+| -------------------------------------------------------------------
+|  Auto-load Models
+| -------------------------------------------------------------------
+| Prototype:
+|
+|	$autoload['model'] = array('first_model', 'second_model');
+|
+| You can also supply an alternative model name to be assigned
+| in the controller:
+|
+|	$autoload['model'] = array('first_model' => 'first');
+*/
+$autoload['model'] = array("");
--- application/views/Pendaftaran/pendaftaranPesertaLomba.php
+++ application/views/Pendaftaran/pendaftaranPesertaLomba.php
@@ -4,6 +4,7 @@
 	<title>Pendaftaran Peserta Lomba - ITFest 4.0</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
 
 	<style type="text/css">
 
@@ -35,15 +36,24 @@
 			<div class="col-6"> <!-- kiri -->
 				<div class="border">
 					<header style="padding: 3px 3px 3px 10px; background-color: #3d3b3b; color: white;"><b>Data Lomba</b></header>
+
+					<!--Form-->
+					<form id="form_pendaftaran">
 					<div style="padding-left: 5px; margin-top: 5px;">
 						<label>Cabang Lomba</label>
 						<div class="form-group">
-							<select class="form-control">
+							<select class="form-control required" name="cabang_lomba" required = true>
 								<option value="" selected disabled>-- Pilih Cabang Lomba --</option>
-								<option value="Competitive Programing">Competitive Programing</option>
-								<option value="Business IT Case">Business IT Case</option>
-								<option value="Application Development">Application Development</option>
-								<option value="UI/UX Design">UI/UX Design</option>
+
+								<!--Nampilin list lomba-->
+								<?php foreach($data_lomba as $key){?>
+
+								<option value="<?php echo $key->id_lomba?>"><?php echo $key->nama_lomba ?></option>
+
+
+								<?php } ?>
+								<!--/Nampilin list lomba-->
+
 							</select>	
 						</div>
 					</div>
@@ -54,35 +64,41 @@
 					<div style="padding-left: 5px; margin-top: 5px;">
 						<label>Nama Tim</label>
 						<div class="form-group">
-							<input type="text" name="nama_tim" placeholder="Nama Tim" class="form-control">	
+							<input type="text" name="nama_team" placeholder="Nama Tim" class="form-control required">	
 						</div>
 					</div>
+
+					<!-- ------------------------------->
+					<div style="padding-left: 5px; margin-top: 5px;">
+						<label>Username</label>
+						<div class="form-group">
+							<input type="text" name="username" placeholder="Username Tim" class="form-control required" id = "username_tim">
+						</div>
+						<i style="color:red" class="warning" id="username_warning_red">username harus terdiri dari 6-12 karakter</i>	
+					</div>
+					<!-- ------------------------------->
+
 					<!-- ------------------------------->
 					<div style="padding-left: 5px; margin-top: 5px;">
 						<label>Password Tim</label>
 						<div class="form-group">
-							<input type="password" name="password_tim" placeholder="Nama Tim" class="form-control">	
+							<input type="password" name="password" id ="password_tim" placeholder="Nama Tim" class="form-control required">	
 						</div>
 					</div>
 					<!-- ------------------------------->
 					<div style="padding-left: 5px; margin-top: 5px;">
 						<label>Konfirmasi Password Tim</label>
 						<div class="form-group">
-							<input type="password" name="konfirmasi_password_tim" placeholder="Konfirmasi Password" class="form-control">	
+							<input type="password" id="konfirmasi_password_tim" placeholder="Konfirmasi Password" class="form-control">	
 						</div>
+						<i style="color:red" class="warning" id="password_warning_red">password yang anda ketik tidak sesuai</i>
 					</div>
+					
 					<!-- ------------------------------->
 					<div style="padding-left: 5px; margin-top: 5px;">
 						<label>Asal Universitas</label>
 						<div class="form-group">
-							<input type="text" name="universitas" placeholder="Asal Universitas" class="form-control">	
-						</div>
-					</div>
-					<!-- ------------------------------->
-					<div style="padding-left: 5px; margin-top: 5px;">
-						<label>Asal Fakultas</label>
-						<div class="form-group">
-							<input type="text" name="tim_fakultas" placeholder="Asal Universitas" class="form-control">	
+							<input type="text" name="universitas_team" placeholder="Asal Universitas" class="form-control required" required>	
 						</div>
 					</div>
 				</div>
@@ -100,12 +116,12 @@
 							
 							<label>Nama Ketua</label>
 							<div class="form-group">
-								<input type="text" name="nama_ketua" placeholder="Nama Ketua" class="form-control">
+								<input type="text" name="nama_ketua" placeholder="Nama Ketua" class="form-control required" required>
 							</div>
 							<!-- --------------------------------------->
 							<label>Jenis Kelamin</label>
 							<div class="form-group">
-								<select class="form-control" name="jenis_kelamin_ketua">
+								<select class="form-control required" name="jenis_kelamin_ketua" required>
 									<option selected disabled value="">--Pilih Jenis Kelamin--</option>
 									<option value="Laki-laki">Laki-laki</option>
 									<option value="Perempuan">Perempuan</option>
@@ -114,33 +130,20 @@
 							<!-- --------------------------------------->
 							<label>No. HP Ketua</label>
 							<div class="form-group">
-								<input type="number" name="no_hp_ketua" placeholder="Nama Ketua" class="form-control">
+								<input type="number" name="no_hp_ketua" placeholder="Nama Ketua" class="form-control required" required>
 							</div>
 							<!-- --------------------------------------->
 							<label>Email Ketua</label>
 							<div class="form-group">
-								<input type="email" name="email_ketua" placeholder="Email" class="form-control">
+								<input type="email" name="email_ketua" placeholder="Email" class="form-control required" required>
 							</div>
 							<!-- --------------------------------------->
 							<div class="border" style="padding: 5px 5px 5px 5px; margin-top: 5px;">
-								<label>Upload File KTM Ketua</label>
+								<label>Upload File Data Pribadi</label>
 								<div class="form-group">
-									<input type="file" id="my_file" name="ktm_ketua">
-								</div>	
-							</div>
-							<!-- --------------------------------------->
-							<div class="border" style="padding: 5px 5px 5px 5px; margin-top: 5px;">
-								<label>Upload File Surat Aktif Ketua</label>
-								<div class="form-group">
-									<input type="file" id="my_file" name="surat_aktif_ketua">
+									<input type="file" id="file_ketua" name="file_ketua" required>
 								</div>
-							</div>
-							<!-- --------------------------------------->
-							<div class="border" style="padding: 5px 5px 5px 5px; margin-top: 5px;">
-								<label>Upload File Foto</label>
-								<div class="form-group">
-									<input type="file" id="my_file" name="foto_ketua">
-								</div>	
+								<p style="font-size: 14px;color:red;">Data pribadi berupa pas foto, surat keterangan aktif kuliah dan scan ktm yang dimasukkan ke dalam ZIP</p>	
 							</div>
 							
 						</div>
@@ -151,15 +154,15 @@
 						<header style="padding: 3px 3px 3px 10px; background-color: #3d3b3b; color: white;">Anggota Tim</header>
 						<div class="border" style="padding: 3px 3px 3px 10px;">
 							
-							
+							<!--Data anggota 1-->
 							<label>Nama Anggota</label>
 							<div class="form-group">
-								<input type="text" name="nama_anggota1" placeholder="Nama Anggota" class="form-control">
+								<input type="text" name="nama_anggota1" id="nama_anggota1" placeholder="Nama Anggota" class="form-control">
 							</div>
 							<!-- --------------------------------------->
 							<label>Jenis Kelamin</label>
 							<div class="form-group">
-								<select class="form-control" name="jenis_kelamin1">
+								<select class="form-control" name="jenis_kelamin_anggota1" id="jenis_kelamin_anggota1">
 									<option selected disabled value="">--Pilih Jenis Kelamin--</option>
 									<option value="Laki-laki">Laki-laki</option>
 									<option value="Perempuan">Perempuan</option>
@@ -168,50 +171,113 @@
 							<!-- --------------------------------------->
 							<label>No. HP</label>
 							<div class="form-group">
-								<input type="number" name="no_hp_anggota1" placeholder="No. HP" class="form-control">
+								<input type="number" name="no_hp_anggota1" placeholder="No. HP" class="form-control" id="no_hp_anggota1">
 							</div>
 							<!-- --------------------------------------->
 							<label>Email</label>
 							<div class="form-group">
-								<input type="email" name="email_anggota1" placeholder="Email" class="form-control">
+								<input type="email" name="email_anggota1" placeholder="Email" class="form-control" id="email_anggota1">
 							</div>
 							<!-- --------------------------------------->
 							<div class="border" style="padding: 5px 5px 5px 5px; margin-top: 5px;">
-								<label>Upload File KTM</label>
+								<label>Upload File Data Pribadi</label>
 								<div class="form-group">
-									<input type="file" id="my_file" name="ktm_anggota1">
+									<input type="file" id="file_anggota1" name="file_anggota1">
 								</div>	
+								<p style="font-size: 14px;color:red;">Data pribadi berupa pas foto, surat keterangan aktif kuliah dan scan ktm yang dimasukkan ke dalam ZIP</p>	
 							</div>
-							<!-- --------------------------------------->
-							<div class="border" style="padding: 5px 5px 5px 5px; margin-top: 5px;">
-								<label>Upload File Surat Aktif</label>
-								<div class="form-group">
-									<input type="file" id="my_file" name="surat_aktif_anggota1">
-								</div>
-							</div>
-							<!-- --------------------------------------->
-							<div class="border" style="padding: 5px 5px 5px 5px; margin-top: 5px;">
-								<label>Upload File Foto</label>
-								<div class="form-group">
-									<input type="file" id="my_file" name="foto_anggota1">
-								</div>	
-							</div>
-							
+							<!--/Data anggota 1-->
 						</div>
 					</div>
+					<button type="submit">Submit</button>
 				</div>
 
 			</div>
 
 		</div>
 	</div>
+	</form>
 	<br>
 	<br>
 	<br>
+<script src = "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script type="text/javascript">
-    document.getElementById('my_file').click();
-	document.getElementById('get_file').onclick = function() {
-};
+	// Variable untuk pengecekan form
+    var valid = true;
+    // Inisialisasi untuk mengetahui jumlah anggota anggota
+    var jumlah_anggota = 0;
+    // Fungsi untuk melakukan pengecekan apakah form kosong atau tidak
+    $(document).ready(function(){
+    	$('.warning').hide();
+
+        $('#username').keyup(function(){
+        	var username = $('#username_tim').val();
+        	if(username.length() == 0 || username.length < 6 || username.length() > 12)
+        		$('#username_warning_red').show('slow');
+        	else
+        		$('#username_warning_red').hide('slow');
+        });
+
+		$('#konfirmasi_password_tim').keyup(function(){
+        	var password = $('#password_tim').val();
+        	var konfirmasi = $('#konfirmasi_password_tim').val();
+        	if(password != konfirmasi)
+        		$('#password_warning_red').show('slow');
+        	else
+        		$('#password_warning_red').hide('slow');
+        });
+
+        $('#password_tim').keyup(function(){
+        	var password = $('#password_tim').val();
+        	var konfirmasi = $('#konfirmasi_password_tim').val();
+        	if(password != konfirmasi)
+        		$('#password_warning_red').show('slow');
+        	else
+        		$('#password_warning_red').hide('slow');
+        });
+
+        $('#form_pendaftaran').submit(function(event){
+        	// Menambah jumlah anggota
+        	if($('#nama_anggota1').val() != ""){
+        		jumlah_anggota++;
+        		$('#nama_anggota1').setAttribute('required','true');
+        		$('#jenis_kelamin_anggota1').setAttribute('required','true');
+        		$('#no_hp_anggota1').setAttribute('required','true');
+        		$('#email_anggota1').setAttribute('required','true');
+        		$('file_anggota1').setAttribute('required','true');
+        	}
+        	//if($('#nama_anggota2').val() != ""){
+        	//	jumlah_anggota++;
+        	//}
+
+        	
+        });
+
+        $('#form_pendaftaran').submit(function(event) {
+        	event.preventDefault(); 
+			$.ajax({
+			url: '<?php echo base_url('index.php/Pendaftaran/daftar_kompetisi') ?>',
+			type: 'POST',
+			data:new FormData(this),
+            processData:false,
+            contentType:false,
+            cache:false,
+            async:false,
+            error: function(data){
+
+            	Swal.fire('Kesalahan!!', 'Gagal menghubungkan ke server !!', 'error')
+            },
+            success: function(data){
+            	if (data==1) {
+            	Swal.fire('Berhasil !!', 'Panitia berhasil ditambahkan !!', 'success')
+            	}
+            	else
+            		Swal.fire('Kesalahan!!', 'Username telah digunakan !!', 'error')
+            }
+		})
+        });
+    });
+
 </script>
 </body>
 </html>
