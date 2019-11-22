dec889d09307336ac50ade10f56024f8b78fdef7
Ali Hidayat <40517187+usokun@users.noreply.github.com>
2019-11-22 18:40:35 +0700

    Tambah beberapa halaman pada admin dan bendahara

---
+++ application/views/bendahara/page/reBerkas.php
@@ -0,0 +1,56 @@
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-12">
+			<h5>Laporan berkas peserta | ITFest4.0 Universitas Sumatera Utara</h5>
+			<hr>
+		</div>
+		<div class="container-fluid">
+			<button class="btn btn-primary" id="tambahDO">Seleksi Berkas</button>
+		</div>
+	</div>
+	<?php foreach ($reBerkas as $key => $Berkas): ?>
+	<?php endforeach ?>
+	<?php foreach ($reSingkat as $key => $Singkat): ?>
+	<?php endforeach ?>
+	<div class="row" style="margin-top: 15px;">
+		<div class="col-6">
+			<div class="card " style="background-color: #7386D5">
+			<div class="card-header text-white">
+				Laporan Berkas
+			</div>
+			<ul class="list-group list-group-flush justify-content-between">
+			    <li class="list-group-item d-flex justify-content-between align-items-center">
+				    Jumlah berkas belum seleksi
+				    <span class="badge badge-primary badge-pill"><?php echo $Berkas->jumlah_new ?></span>
+				</li>
+			    <li class="list-group-item d-flex justify-content-between align-items-center">
+				    Jumlah berkas diterima
+				    <span class="badge badge-primary badge-pill"><?php echo $Berkas->jumlah_lulus ?></span>
+				</li>
+				<li class="list-group-item d-flex justify-content-between align-items-center">
+				    Jumlah berkas ditolak
+				    <span class="badge badge-primary badge-pill"><?php echo $Berkas->jumlah_fail ?></span>
+				</li>
+			</ul>
+			</div>
+		</div>
+		<div class="col-6">
+			<div class="card " style="background-color: #7386D5">
+			<div class="card-header text-white">
+				Laporan Tim
+			</div>
+			<ul class="list-group list-group-flush justify-content-between">
+			    <li class="list-group-item d-flex justify-content-between align-items-center">
+				    Jumlah Tim mendaftar
+				    <span class="badge badge-primary badge-pill"><?php echo $Berkas->jumlah ?></span>
+				</li>
+			    <li class="list-group-item d-flex justify-content-between align-items-center">
+				    Jumlah Tim belum verifikasi pembayaran
+				    <span class="badge badge-primary badge-pill"><?php echo $Singkat->jumlah_bayar ?></span>
+				</li>
+			</ul>
+			</div>
+		</div>
+		
+	</div>
+</div>
---
+++ application/views/bendahara/page/reSingkat.php
@@ -0,0 +1,125 @@
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-12">
+			<h5>Dashboard | ITFest4.0 Universitas Sumatera Utara</h5>
+			<hr>
+			
+		</div>
+	</div>
+	<div class="row" style="margin-top: 0px;">
+		<?php foreach ($reTahap as $key => $Ddash): ?>
+			
+			
+		<?php endforeach ?>
+
+	<div class="col-4">
+		<div class="card text-white bg-info mb-3" style="max-width: 18rem;">
+		    <div class="card-header text-capitalize text-center">Jumlah Tim</div>
+		  	<div class="card-body">
+		  		<div>
+		    		<h1 class="card-title text-center">
+						<?php echo $Ddash->jumlah_tim ?>
+		    		</h1>
+	    		</div>
+	  		</div>
+		</div>
+	</div>
+	<div class="col-4">
+		<div class="card text-white bg-info mb-3" style="max-width: 18rem;">
+	  	<div class="card-header text-capitalize text-center">Jumlah Tahap Seleksi</div>
+	  		<div class="card-body">
+	  			<div>
+	    			<h1 class="card-title text-center">
+						<?php echo $Ddash->jumlah_tahap ?>
+	    			</h1>
+	    		</div>
+	  		</div>
+		</div>
+	</div>
+	<div class="col-4">
+		<div class="card text-white bg-info mb-3" style="max-width: 18rem;">
+	  	<div class="card-header text-capitalize text-center">Jumlah Berkas belum di cek</div>
+	  		<div class="card-body">
+	  			<div>
+	    			<h1 class="card-title text-center">
+						<?php echo $Ddash->jumlah_blmver ?>
+	    			</h1>
+	    		</div>
+	  		</div>
+		</div>
+	</div>
+	<div class="col-4">
+		<div class="card text-white bg-success mb-3" style="max-width: 18rem;">
+	  	<div class="card-header text-capitalize text-center">Jumlah Tim sudah bayar</div>
+	  		<div class="card-body">
+	  			<div>
+	    			<h1 class="card-title text-center">
+						<?php echo $Ddash->jumlah_bayar  ?>
+	    			</h1>
+	    		</div>
+	  		</div>
+		</div>
+	</div>
+	<div class="col-4">
+		<div class="card text-white bg-success mb-3" style="max-width: 18rem;">
+	  	<div class="card-header text-capitalize text-center">Jumlah Tim lulus berkas</div>
+	  		<div class="card-body">
+	  			<div>
+	    			<h1 class="card-title text-center">
+						<?php echo $Ddash->jumlah_suksesSeleksi ?>
+	    			</h1>
+	    		</div>
+	  		</div>
+		</div>
+	</div>
+	<div class="col-4">
+		<div class="card text-white bg-success mb-3" style="max-width: 18rem;">
+	  	<div class="card-header text-capitalize text-center">Total Peserta</div>
+	  		<div class="card-body">
+	  			<div>
+	    			<h1 class="card-title text-center">
+						<?php echo $Ddash->total_peserta ?>
+	    			</h1>
+	    		</div>
+	  		</div>
+		</div>
+	</div>
+	<div class="col-4">
+		<div class="card text-white bg-danger mb-3" style="max-width: 18rem;">
+	  	<div class="card-header text-capitalize text-center">Berkas ditolak</div>
+	  		<div class="card-body">
+	  			<div>
+	    			<h1 class="card-title text-center">
+						<?php echo $Ddash->jumlah_tolak ?>
+	    			</h1>
+	    		</div>
+	  		</div>
+		</div>
+	</div>
+	
+	<div class="col-4">
+		<div class="card text-white bg-danger mb-3" style="max-width: 18rem;">
+	  	<div class="card-header text-capitalize text-center">Tim belum bayar</div>
+	  		<div class="card-body">
+	  			<div>
+	    			<h1 class="card-title text-center">
+						<?php $nilai1 = $Ddash->jumlah_tim-$Ddash->jumlah_bayar; echo $nilai1;  ?>
+	    			</h1>
+	    		</div>
+	  		</div>
+		</div>
+	</div>
+	<div class="col-4">
+		<div class="card text-white bg-danger mb-3" style="max-width: 18rem;">
+	  	<div class="card-header text-capitalize text-center">Total Tim gugur</div>
+	  		<div class="card-body">
+	  			<div>
+	    			<h1 class="card-title text-center">
+						<?php $nilai = $Ddash->jumlah_tolak+$nilai1; echo $nilai; ?>
+	    			</h1>
+	    		</div>
+	  		</div>
+		</div>
+	</div>
+	</div>
+	</div>
