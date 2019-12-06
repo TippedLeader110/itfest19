f798d413803a40380f38aac636788e1b3e3f0348
Bayhaqi101 <39187403+TippedLeader110@users.noreply.github.com>
2019-12-06 14:40:42 +0700

    Update Admin.php

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -288,14 +288,12 @@ class Admin extends CI_Controller {
 		$this->loginProtocol();
 		$dataGet = $this->adminModel->getSingkat();
 		$dataGet2 = $this->adminModel->getSingkat2();
-		var_dump($dataGet);
-		die;
-		// $data = [
-		// 	'title' => 'Laporan Singkat',
-		// 	'reTahap' => $dataGet,
-		// 	'reTahapkompe' => $dataGet2
-		// ];
-		// $this->load->view('admin/page/reSingkat', $data);
+		$data = [
+			'title' => 'Laporan Singkat',
+			'reTahap' => $dataGet,
+			'reTahapkompe' => $dataGet2
+		];
+		$this->load->view('admin/page/reSingkat', $data);
 	}
 
 	public function doHapusPanitia()
