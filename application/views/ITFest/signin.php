37d616edfa827a270d630a54e6c09f611255df88
Ilham Kurnia Wahyudi Rusli <32015538+ilhamkwr@users.noreply.github.com>
2019-12-08 22:51:01 +0700

    Update signin.php

--- application/views/ITFest/signin.php
+++ application/views/ITFest/signin.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Login ITFest 4.0</title>
+	<title>Login ITFest 2020</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
