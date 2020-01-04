36af416fa7707e5466340f770efaa4c635e2dc88
Bayhaqi101 <39187403+TippedLeader110@users.noreply.github.com>
2020-01-04 21:58:45 +0700

    Update qrgenerate.php

--- application/views/peserta/qrgenerate.php
+++ application/views/peserta/qrgenerate.php
@@ -5,12 +5,17 @@
 	<script type="text/javascript" src="<?php echo base_url('assets/js/jquery.min.js') ?>"></script>
 	<script type="text/javascript" src="<?php echo base_url('assets/js/jquery-barcode.min.js') ?>"></script>
 	<script type="text/javascript" src="<?php echo base_url('assets/js/jqueryqr.js') ?>"></script>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('/assets/css/bootstrap.min.css') ?>">
 </head>
 <body>
 	<div style="width: 500px;">
-		<div>
-			<div id="qr">
-				
+		<div class="row">
+			<div class="col-md-8 offset-md-1 col-12">
+				Jika QR tidak muncul tolong matikan fitur LITE dari Google Chrome <br>
+				<button onClick="window.print()" class="btn btn-primary">Page to PDF</button>
+				<div id="qr" style="margin-top: 10px;">
+					
+				</div>
 			</div>
 		</div>
 		<script type="text/javascript">
