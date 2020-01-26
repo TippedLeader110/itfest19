14c856d7af9c96b2c38207a4d0eac88d6f4090ea
Bayhaqi101 <39187403+TippedLeader110@users.noreply.github.com>
2020-01-26 11:33:59 +0700

    Timeline update

--- application/views/ITFest/index.php
+++ application/views/ITFest/index.php
@@ -201,7 +201,7 @@
 						<hr class="hr-timeline"><br>
 					</div>
 					<div class="text-timeline">
-						<p><b>9 Desember 2019 – 11 Januari 2020</b> Registrasi Kompetisi</p>
+						<p><b>9 Desember 2019 – 25 Januari 2020</b> Registrasi Kompetisi</p>
 						<p><b>14 Januari 2020 – 9 Februari 2020</b> Penyisihan I</p>
 						<p><b>10 Februari 2020 – 16 Februari 2020</b> Penjurian & Pengumuman I</p>
 						<p><b>17 Februari 2020 – 09 Maret 2020</b> Penyisihan II</p>
