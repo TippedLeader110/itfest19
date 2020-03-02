0f33b1dd9b1538d1ec91d15736ae18f90026a0ff
Bayhaqi101 <39187403+TippedLeader110@users.noreply.github.com>
2020-03-02 23:00:31 +0700

    Update email3.php

--- email3.php
+++ email3.php
@@ -8,98 +8,10 @@
         <img src="http://itfestusu.id/assets/images/logo.png" width="150px">
           <p>Hi, <span class="user" style="font-weight: 500;">'.$nama.'</span>!</p>
           <p>
-            Terima kasih telah melakukan pembayaran tiket <span class="text-seminar" style="font-weight: 600;
-        color: #2b2c41;">Seminar Nasional ITFest USU 2020</span>.
+            Terjadi kesalahan / bukti pembayaran yang anda kirim tidak valid !! <br>
+            Untuk informasi lebih lanjut anda dapat menghubungi cs : itfestusu@gmail.com .
           </p>
-          <p>
-            Bukti pembayaran anda telah di terima dan terverifikasi! <br>
-            Silahkan mendownload QR e-tiket yang kami sediakan pada tautan berikut.
-          </p>
-
-          <div class="col text-center">
-            <a  name="link_cetak_tiket" class="btn btn-dark" href="http://itfestusu.id/Verifikasi/generate?api-key='.$qr.'" style="padding: 13px 13px 13px 13px;
-        border-radius: 5px;
-        background-color: #514a9d;
-        text-decoration: none;
-        color: white;">Download QR e-Tiket Sekarang</a>
-          </div>
-          <br>
-
-          <div class="detail-head" style="font-weight: 500;
-        margin-top: 10px;">
-            Berikut detail pesanan Anda:
-          </div>
-
-          <hr>
-          <table class="">
-            <tr>
-              <td style="width: 20%;">Event Name</td>
-              <td style="width: 2%;">:</td>
-              <td style="width: 83%;">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit</td>
-            </tr>
-            <tr>
-              <td scope="col">Event Location</td>
-              <td>:</td>
-              <td>Auditorium USU</td>
-            </tr>
-            <tr>
-              <td scope="col">Event Schedule</td>
-              <td>:</td>
-              <td>4 Mei 2020 10.00 - 16.00</td>
-            </tr>
-            <tr>
-              <td scope="col">Invoice Code</td>
-              <td>:</td>
-              <td>'.$id.'</td>
-            </tr>
-            <tr>
-              <td scope="col">Event Status</td>
-              <td>:</td>
-              <td>PAID</td>
-            </tr>
-            <tr>
-              <td scope="col">Invoice Date</td>
-              <td>:</td>
-              <td>27 April 2020 10.23</td>
-            </tr>
-            <tr>
-              <td scope="col">Payment Type</td>
-              <td>:</td>
-              <td>ATM Transfer</td>
-            </tr>
-          </table>
-
-          <div class="detail-head" style="font-weight: 500;
-        margin-top: 10px;">
-            Detail Pesanan Anda:
-          </div>
-          <hr>
-          <table class="table" border="1" style="padding: 15px;
-    border: 1px solid #ddd;
-    text-align: left;
-    border-collapse: collapse;
-    width: 100%;
-    background-color: white;">
-            <tr>
-              <th style="padding: 15px;">Ringkasan</th style="padding: 15px;">
-              <th style="padding: 15px;">Harga per Tiket</th style="padding: 15px;">
-              <th style="padding: 15px;">Jumlah</th style="padding: 15px;">
-            </tr>
-            <tr>
-              <td style="padding: 15px;">Event show: 10.00 - 16.00</td style="padding: 15px;">
-              <td style="padding: 15px;">Rp 60.000,00</td style="padding: 15px;">
-              <td style="padding: 15px;">Rp 60.000,00</td style="padding: 15px;">
-            </tr>
-            <tr>
-              <td style="padding: 15px;" colspan="2" class="total">Grand Total</td style="padding: 15px;">
-              <td style="padding: 15px;" class="total" style="font-weight: 700;">Rp 60.000,00</td style="padding: 15px;">
-            </tr>
-          </table>
-          <p>Jika ada keluhan anda dapat menghubungi cs : itfestusu@gmail.com</p>
-          <br>
-          <p>
-            Terima Kasih.
-          </p>
+          <p>Terima Kasih.</p>
         </div>
       </div>
     </div>
